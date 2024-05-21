ca0b810e4708a3af80a6e24b7c6369c254b8fb17
sandrockjustin <sandrock.justin@gmail.com>
2024-05-21 14:40:31 -0500

    +modified getObjectValues()

--- day-2/loops.js
+++ day-2/loops.js
@@ -75,7 +75,9 @@ function getObjectValues(object) {
   var newArray = [];
 
   for (var key in object){
-    newArray.push(object.key);
+
+    newArray.push(object[key]);
+
   }
   
   return newArray; 

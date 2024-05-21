358e0d9bd1068bdf917bb4b7dd82c3e04f68e142
sandrockjustin <sandrock.justin@gmail.com>
2024-05-21 14:10:36 -0500

    +completed FizzBuzz

--- day-2/homework/fizz-buzz.js
+++ day-2/homework/fizz-buzz.js
@@ -12,9 +12,27 @@
 */
 function fizzBuzz() {
     // YOUR CODE GOES BELOW HERE //
+    for (var i = 0; i <= 100; i++){
 
+        if ((i % 5 === 0) && (i % 3 === 0)) {
 
+            console.log("FizzBuzz");
 
+        } else if (i % 5 === 0) {
+
+            console.log("Buzz");
+
+        } else if (i % 3 === 0) {
+
+            console.log("Fizz");
+
+        } else {
+
+            console.log(i);
+
+        }
+
+    }
     // YOUR CODE GOES ABOVE HERE //
 }
 
